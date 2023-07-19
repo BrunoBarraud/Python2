c1ddb150158299abcec4377881ed6e4e3faf5231
BrunoBarraud <brunobarraud15@gmail.com>
2023-07-19 08:38:18 -0300

    fix

--- AbrahamArbol.py
+++ AbrahamArbol.py
@@ -6,8 +6,10 @@ En el proceso debe estar el algoritmo de ordenación llamado “Burbuja”. Se p
 """
 
 #Respuesta 1
-class Tree:
-    nombres = ""
+
+
+class Arbol: 
+    nombre = ''
     concubina = []
     hijos = []
 
@@ -16,106 +18,45 @@ class Tree:
         self.concubina = []
         self.hijos = []
 
-node = Tree('Abraham')
-node.concubina.append(Tree('Agar'))
-node.concubina.append(Tree('Sara'))
+nodo = Arbol('Abraham')
+nodo.concubina.append(Arbol('Agar'))
+nodo.concubina.append(Arbol('Sara'))
 
-node.concubina[0].hijos.append(Tree('Ismael'))
-node.concubina[1].hijos.append(Tree('Isaac'))
-node.concubina[1].hijos[0].concubina.append(Tree('Rebeca'))
+nodo.concubina[0].hijos.append(Arbol('Ismael'))
+nodo.concubina[1].hijos.append(Arbol('Isaac'))
+nodo.concubina[1].hijos[0].concubina.append(Arbol('Rebeca'))
 
-node.concubina[1].hijos[0].concubina[0].hijos.append(Tree('Esau'))
-node.concubina[1].hijos[0].concubina[0].hijos.append(Tree('Jacob'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina.append(Tree('Lea'))
+nodo.concubina[1].hijos[0].concubina[0].hijos.append(Arbol('Esau'))
+nodo.concubina[1].hijos[0].concubina[0].hijos.append(Arbol('Jacob'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina.append(Arbol('Lea'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina.append(Arbol('Zilpa'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina.append(Arbol('Raquel'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina.append(Arbol('Bilha'))
 
-#Hijos Lea
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Tree('Ruben'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Tree('Simeon'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Tree('Levi'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Tree('Juda'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Tree('Isacar'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Tree('Zabulon'))
+# Hijos de Lea
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Ruben'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Simeon'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Levi'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Juda'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Isacar'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Zabulon'))
 
-#Hijos Zilpa
-node.concubuna[1].hijos[0].concubina[0].hijos[1].concubina[1].hijos.append(Tree('Gad'))
-node.concubuna[1].hijos[0].concubina[0].hijos[1].concubina[1].hijos.append(Tree('Aser'))
+# Hijos de Zilpa 
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[1].hijos.append(Arbol('Gad'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[1].hijos.append(Arbol('Aser'))
 
-#Hijos Raquel
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos.append(Tree('Jose'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos.append(Tree('Benjamin'))
+# Hijos de Raquel 
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos.append(Arbol('Jose'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos.append(Arbol('Benjamin'))
 
-#Hijos Bilha
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[3].hijos.append(Tree('Dan'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[3].hijos.append(Tree('Neftali'))
+# Hijos de Bilha
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[3].hijos.append(Arbol('Dan'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[3].hijos.append(Arbol('Neftali'))
 
-#Familia de José
-
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos[0].concubina.append(Tree('Asenat'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos[0].concubina[0].hijos.append(Tree('Manases'))
-node.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos[0].concubina[0].hijos.append(Tree('Efrain'))
-
-#Respuesta 2: Recorrer el árbol genealógico de forma recursiva e ir mostrando a sus miembros por pantalla de forma clara y ordenada usando sangrías. (45pts)
-
-def recorrido(r, sangria = ''):
-    print(sangria + r.nombre)
-    aux = sangria
-    sangria += "   "
-    for j in r.hijos:
-        recorrido(j, sangria)
-    for i in r.concubina:
-        recorrido(i, aux)
-    
-
-
-# class Arbol: 
-#     nombre = ''
-#     concubina = []
-#     hijos = []
-
-#     def __init__(self, n):
-#         self.nombre = n
-#         self.concubina = []
-#         self.hijos = []
-
-# nodo = Arbol('Abraham')
-# nodo.concubina.append(Arbol('Agar'))
-# nodo.concubina.append(Arbol('Sara'))
-
-# nodo.concubina[0].hijos.append(Arbol('Ismael'))
-# nodo.concubina[1].hijos.append(Arbol('Isaac'))
-# nodo.concubina[1].hijos[0].concubina.append(Arbol('Rebeca'))
-
-# nodo.concubina[1].hijos[0].concubina[0].hijos.append(Arbol('Esau'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos.append(Arbol('Jacob'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina.append(Arbol('Lea'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina.append(Arbol('Zilpa'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina.append(Arbol('Raquel'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina.append(Arbol('Bilha'))
-
-# # Hijos de Lea
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Ruben'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Simeon'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Levi'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Juda'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Isacar'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[0].hijos.append(Arbol('Zabulon'))
-
-# # Hijos de Zilpa 
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[1].hijos.append(Arbol('Gad'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[1].hijos.append(Arbol('Aser'))
-
-# # Hijos de Raquel 
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos.append(Arbol('Jose'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos.append(Arbol('Benjamin'))
-
-# # Hijos de Bilha
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[3].hijos.append(Arbol('Dan'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[3].hijos.append(Arbol('Neftali'))
-
-# # Familia Jose
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos[0].concubina.append(Arbol('Asenat'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos[0].concubina[0].hijos.append(Arbol('Manases'))
-# nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos[0].concubina[0].hijos.append(Arbol('Efrain'))
+# Familia Jose
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos[0].concubina.append(Arbol('Asenat'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos[0].concubina[0].hijos.append(Arbol('Manases'))
+nodo.concubina[1].hijos[0].concubina[0].hijos[1].concubina[2].hijos[0].concubina[0].hijos.append(Arbol('Efrain'))
 
 def recorrer(r, sangria = ''):
     print(sangria + r.nombre)
@@ -130,16 +71,16 @@ recorrer(nodo)
 
 def listar():
     nombres = []
-    listarTodos(nodo, nombres)
+    listar_todos(nodo, nombres)
     return nombres
 
 
-def listarTodos(r, nombres):
+def listar_todos(r, nombres):
     nombres.append(r.nombre)
     for j in r.hijos: 
-        listarTodos(j, nombres)
+        listar_todos(j, nombres)
     for i in r.concubina: 
-        listarTodos(i, nombres)
+        listar_todos(i, nombres)
 
 def ordenar_burbuja(lista):
     for i in range(len(lista)-1):
